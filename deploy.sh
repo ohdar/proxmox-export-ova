51fa5a78ba233507499834d4bcb56f1ee83f052d
Brajesh Ohdar <brajesh@localhost.localdomain>
2025-06-06 16:39:28 +0530

    commit: change to deploy Export to OVA integration

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,15 @@ echo "Installing the .deb package..."
 dpkg -i pve-export-ova.deb
 
 echo "Copying ExportOva.pm to API2 directory..."
-cp patch/PVE_API2_Qemu_ExportOva.pm /usr/share/perl5/PVE/API2/Qemu/ExportOva.pm
+# cp patch/PVE_API2_Qemu_ExportOva.pm /usr/share/perl5/PVE/API2/Qemu/ExportOva.pm
+cp api/PVE_API2_Qemu_ExportOva.pm /usr/share/perl5/PVE/API2/Qemu/ExportOva.pm
 
 echo "Backing up original Qemu.pm..."
 cp /usr/share/perl5/PVE/API2/Qemu.pm /usr/share/perl5/PVE/API2/Qemu.pm.bak
 
 echo "Applying patch to Qemu.pm..."
-patch /usr/share/perl5/PVE/API2/Qemu.pm < patch/Qemu_pm.patch
+# patch /usr/share/perl5/PVE/API2/Qemu.pm < patch/Qemu_pm.patch
+patch /usr/share/perl5/PVE/API2/Qemu.pm < api/Qemu_pm.patch
 
 echo "Restarting pveproxy service..."
 systemctl restart pveproxy
